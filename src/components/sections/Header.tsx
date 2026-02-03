175be0a64a4d7980bb879d56dd7b0bb7441688ac
mahenti <itmahenti@gmail.com>
2026-02-03 23:59:14 +0400

    Add slight padding to left of logo

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
       <div className="max-w-7xl mx-auto px-6 lg:px-8 py-1">
         <div className="flex items-center justify-between">
           {/* Logo */}
-          <a href="/" className="flex items-center -ml-32 lg:-ml-40">
+          <a href="/" className="flex items-center -ml-24 lg:-ml-32">
             <img 
               src="/logotranspe.png" 
               alt="FeedElvi Logo" 
